46f3ad6b6158636ca4a2cd77cfc265f53b4d012f
ml.sftwrs developer <dev@mlsftwrs.ml>
2019-05-29 02:04:31 -0400

    dev_module

--- res/mongo_install.sh
+++ res/mongo_install.sh
@@ -14,3 +14,23 @@ echo "mongodb-org-shell hold" | sudo dpkg --set-selections
 echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
 echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 
+### MONGO-C-DRIVER INSTALLATION ###
+
+CWD=$(pwd)
+INS_DIR=mongoc_driver
+
+mkdir ${CWD}/${INS_DIR}
+cd ${CWD}/${INS_DIR}
+
+git clone https://github.com/mongodb/mongo-c-driver.git
+cd mongo-c-driver
+
+python build/calc_release_version.py > VERSION_CURRENT
+mkdir cmake-build
+cd cmake-build
+
+cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
+make
+sudo make install
+
+cd ${CWD} && rm -rf ${INS_DIR}
